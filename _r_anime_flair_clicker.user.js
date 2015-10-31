e85e20d1e661b3dab5445f409fc78026ed9dbd26
cwang22 <wcg0304@sina.com>
2015-10-31 19:43:35 +1100

    auto select flair URL

--- _r_anime_flair_clicker.user.js
+++ _r_anime_flair_clicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         /r/anime flair clicker
 // @namespace    http://seewang.me/
-// @version      1.0
+// @version      1.0.1
 // @description  no more struggle to copy flair URLs by clicking /r/anime to toggle 
 // @author       cwang22
 // @match        https://www.reddit.com/r/anime/*
@@ -11,4 +11,19 @@
 GM_addStyle(".click {width: auto; height: 14px;line-height: 13px;padding: 0 6px;background-image: none;background-color: #9ac;border: 1px solid #aaa;color: #3e5267;cursor: default;font-size: 11px !important;}.flair.click:hover{background-color: #9ac;}");
 $(".flair").click(function(){
     $(this).toggleClass("click");
+    selectElement($(this).get(0));
 });
+
+function selectElement(element) {
+    if (window.getSelection) {
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        sel.addRange(range);
+    } else if (document.selection) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(element);
+        textRange.select();
+    }
+}
